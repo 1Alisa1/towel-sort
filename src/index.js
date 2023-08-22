5f7ca44a2d337724c9c5d48a1d788f39a0b3c9de
Alisa <Alisa.dragun@mail.ru>
2023-08-22 16:19:16 +0200

    done task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix && matrix.length > 0) {
+    return matrix.map((el, i) => i % 2 === 0 ? el : el.reverse()).flat(1);
+  } else {
+    return [];
+  }
+  
 }
